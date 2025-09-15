4eccdf2354546c4948eac86674f77ab14025b376
Ashutosh Kumar <112158814+devilhours@users.noreply.github.com>
2025-09-15 22:29:13 +0530

    Update axios.js

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chatapp-backend-4jwx.onrender.com/",
+  baseURL: import.meta.env.MODE === "development" ? "https://chatapp-backend-4jwx.onrender.com/api" : "/api",
   withCredentials: true,
 });
