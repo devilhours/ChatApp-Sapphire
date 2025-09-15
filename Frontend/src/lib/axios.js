a0fd85fb99607ca9eb5d1c35f627dd58e8fd1b91
Ashutosh Kumar <112158814+devilhours@users.noreply.github.com>
2025-09-15 21:28:39 +0530

    Change axios baseURL for development environment

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chatapp-backend-4jwx.onrender.com" : "/api",
   withCredentials: true,
 });
