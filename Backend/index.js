e406ce6c34d11e3a0dec1e4ae2a57d252c4a239b
devilhours <theashutoshkumar2510@gmail.com>
2025-09-15 17:46:45 +0530

    fixes bugs

--- Backend/index.js
+++ Backend/index.js
@@ -36,7 +36,10 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
-  connectDB();
-});
+
+// server.listen(PORT, () => {
+//   console.log("server is running on PORT:" + PORT);
+//   connectDB();
+// });
+
+export default app;
