9a304762c1331238458a0473e61e3164fc8441d4
Ashutosh Kumar <112158814+devilhours@users.noreply.github.com>
2025-09-17 13:41:21 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,15 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { SocketContextProvider } from "./lib/SocketContextProvider.js"; // 1. Import it
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* 2. Wrap your App with the SocketContextProvider */}
-      <SocketContextProvider>
-        <App />
-      </SocketContextProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
