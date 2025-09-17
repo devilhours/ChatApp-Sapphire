5774ccf57813be33975cc5156402680e33859fa3
Ashutosh Kumar <112158814+devilhours@users.noreply.github.com>
2025-09-17 13:21:49 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { SocketContextProvider } from "./lib/SocketContextProvider.jsx"; // 1. Import it
+import { SocketContextProvider } from "./lib/SocketContextProvider.js"; // 1. Import it
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
